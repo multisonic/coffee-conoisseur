67a917a32db09814524bf50604bd6c12e65a0481
Michael aZaleski <me@mikezaleski.com>
2022-02-17 18:38:39 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", paddingBottom: "20px" }}>
+      &copy; 2022 Mike
+    </footer>
+  );
+}
