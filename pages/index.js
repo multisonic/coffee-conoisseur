0882d12c3fb9f2ca70a4db7cd2b5c1023f092724
Michael aZaleski <me@mikezaleski.com>
2022-02-17 23:05:56 -0500

    33. Banner Component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/banner";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log("You clicked me!");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +17,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Conoisseur</h1>
+        {/* <h1 className={styles.title}>Coffee Conoisseur</h1> */}
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
     </div>
   );
