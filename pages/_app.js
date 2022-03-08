907ecbbd7e51952ed59191a95649c909a91dcca6
Mike Zaleski <me@mikezaleski.com>
2022-03-08 10:46:04 -0500

    43. Style our homepage

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
 
 export default MyApp;
-
-function Footer() {
-  return (
-    <footer style={{ textAlign: "center", paddingBottom: "20px" }}>
-      &copy; 2022 Mike
-    </footer>
-  );
-}
